8153dc0a8e79908c0f44f590ad27d2aaaa6d3589
pravin bagul <bagul.pravin2@gmail.com>
2021-02-26 22:17:10 +0530

    WelCome Message

---
+++ addessusingstream.java
@@ -0,0 +1,14 @@
+package com.address.book.stream;
+
+public class AddressBookStr {
+
+	public static void main(String args[]) {
+
+		System.out.println("Welcome To Address Book Programmin");
+		MenuBar obj = new MenuBar();
+		obj.optionChoice();
+
+	}
+
+}
+
